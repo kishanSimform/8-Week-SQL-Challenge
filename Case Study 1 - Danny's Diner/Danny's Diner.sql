7d254ce6bd8b16db3078e27f79d37de5065e3e12
Kishan <kishan.modasiya@simformsolutions.com>
2023-04-25 17:42:59 +0530

    Modification in Queries of case study 1
    Improve some queries and added identation.

--- Case Study 1 - Danny's Diner/Danny's Diner.sql
+++ Case Study 1 - Danny's Diner/Danny's Diner.sql
@@ -2,13 +2,13 @@
 -- Case Study Questions --
 --------------------------
 
-USE [Week 1 - Danny's Diner]
+USE [Week 1 - Danny's Diner];
 
 -- 1. What is the total amount each customer spent at the restaurant?
 SELECT s.customer_id, SUM(price) AS [Total Amount]
 FROM sales s 
 JOIN menu m
-ON s.product_id = m.product_id
+	ON s.product_id = m.product_id
 GROUP BY s.customer_id;
 GO
 
@@ -22,13 +22,14 @@ GO
 WITH cte AS (
 	SELECT customer_id, m.product_name,
 		DENSE_RANK() OVER(PARTITION BY customer_id 
-		ORDER BY order_date) AS [First Item]
+			ORDER BY order_date) AS [First Item]
 	FROM sales s 
 	JOIN menu m
-	ON s.product_id = m.product_id )
+		ON s.product_id = m.product_id 
+	)
 SELECT DISTINCT customer_id, product_name
 FROM cte
-WHERE [First Item] = 1
+WHERE [First Item] = 1;
 GO
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
@@ -37,21 +38,22 @@ WITH cte AS (
 		s.product_id, m.product_name
 	FROM sales s
 	JOIN menu m
-	ON s.product_id = m.product_id
+		ON s.product_id = m.product_id
 	GROUP BY s.product_id, m.product_name
 	ORDER BY COUNT(customer_id) DESC
 	)
-SELECT s.customer_id, c.product_name, COUNT(*) AS [Total Purchased]
+SELECT s.customer_id, c.product_name, 
+	COUNT(*) AS [Total Purchased]
 FROM sales s
 JOIN cte c
-ON s.product_id = c.product_id
+	ON s.product_id = c.product_id
 GROUP BY s.customer_id, c.product_name;
 GO
 
 -- 5. Which item was the most popular for each customer?
 WITH cte AS (
 	SELECT customer_id, product_id, 
-			RANK() OVER (PARTITION BY customer_id 
+		RANK() OVER (PARTITION BY customer_id 
 			ORDER BY COUNT(product_id) DESC) [Popular]
 	FROM sales
 	GROUP BY customer_id, product_id
@@ -59,7 +61,7 @@ WITH cte AS (
 SELECT customer_id, m.product_name
 FROM cte c
 JOIN menu m
-ON c.product_id = m.product_id
+	ON c.product_id = m.product_id
 WHERE Popular = 1;
 GO
 
@@ -67,11 +69,11 @@ GO
 -- 6. Which item was purchased first by the customer after they became a member?
 WITH cte AS (
 	SELECT customer_id, m.product_name, s.order_date, 
-			RANK() OVER (PARTITION BY customer_id
+		RANK() OVER (PARTITION BY customer_id
 			ORDER BY order_date) AS [Row]
 	FROM sales s 
 	JOIN menu m
-	ON s.product_id = m.product_id
+		ON s.product_id = m.product_id
 	WHERE order_date >= 
 		(SELECT join_date FROM members m 
 		WHERE s.customer_id = m.customer_id)
@@ -84,13 +86,14 @@ GO
 -- 7. Which item was purchased just before the customer became a member?
 WITH cte AS (
 	SELECT customer_id, m.product_name, s.order_date, 
-			RANK() OVER (PARTITION BY customer_id
+		RANK() OVER (PARTITION BY customer_id
 			ORDER BY order_date desc) AS [Row]
 	FROM sales s 
 	JOIN menu m
-	ON s.product_id = m.product_id
+		ON s.product_id = m.product_id
 	WHERE order_date <
-		(SELECT join_date FROM members m 
+		(SELECT join_date 
+		FROM members m 
 		WHERE s.customer_id = m.customer_id)
 	)
 SELECT customer_id, product_name, order_date 
@@ -99,47 +102,48 @@ WHERE Row = 1;
 GO
 
 -- 8. What is the total items and amount spent for each member before they became a member?
-SELECT customer_id, COUNT(s.product_id) [Total Items], SUM(price) [Total Amount]
+SELECT customer_id, 
+	COUNT(s.product_id) [Total Items], 
+	SUM(price) [Total Amount]
 FROM sales s
 JOIN menu m
-ON s.product_id = m.product_id
+	ON s.product_id = m.product_id
 WHERE order_date < 
-		(SELECT join_date FROM members m 
+		(SELECT join_date 
+		FROM members m 
 		WHERE s.customer_id = m.customer_id)
 GROUP BY customer_id;
 GO
 
--- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - 
+-- how many points would each customer have?
 SELECT customer_id, 
-		SUM(
-			CASE 
+	SUM(CASE 
 			WHEN m.product_name = 'sushi' 
 				THEN m.price * 20
 			ELSE m.price * 10 
-			END
-		) AS [Points]
+		END) AS [Points]
 FROM sales s
 JOIN menu m
-ON s.product_id = m.product_id
+	ON s.product_id = m.product_id
 GROUP BY customer_id;
 GO
 
--- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi -
+-- how many points do customer A and B have at the end of January?
 SELECT s.customer_id, 
-		SUM(
-			CASE 
+	SUM(CASE 
 			WHEN m.product_name = 'sushi' 
 				THEN price * 20
 			WHEN order_date BETWEEN join_date AND DATEADD(DAY, 6, join_date)
 				THEN price * 20
 			ELSE price * 10
-			END
-		) AS [Total Points]
+	END) AS [Total Points]
 FROM sales s
 JOIN members ms
-ON s.customer_id= ms.customer_id
+	ON s.customer_id= ms.customer_id
 JOIN menu m
-ON s.product_id = m.product_id
+	ON s.product_id = m.product_id
 WHERE s.order_date < '2021-01-31'
 GROUP BY s.customer_id;
 GO
@@ -155,12 +159,12 @@ SELECT s.customer_id, order_date, m.product_name, m.price,
 		WHEN order_date >= join_date AND s.customer_id = ms.customer_id
 			THEN 'Y'
 		ELSE 'N'
-	END  AS [member]
+	END AS [member]
 FROM sales s
 JOIN menu m
-ON s.product_id = m.product_id
+	ON s.product_id = m.product_id
 LEFT JOIN members ms
-ON s.customer_id = ms.customer_id
+	ON s.customer_id = ms.customer_id
 ORDER BY 1, 2, 3;
 GO
 
@@ -171,18 +175,18 @@ WITH cte AS (
 			WHEN order_date >= join_date AND s.customer_id = ms.customer_id
 				THEN 'Y'
 			ELSE 'N'
-		END  AS [member]
+		END AS [member]
 	FROM sales s
 	JOIN menu m
-	ON s
-	.product_id = m.product_id
+		ON s.product_id = m.product_id
 	LEFT JOIN members ms
-	ON s.customer_id = ms.customer_id
+		ON s.customer_id = ms.customer_id
 	)
 SELECT customer_id, order_date, product_name, price, member,
 	CASE member
 		WHEN 'Y'
-			THEN RANK() OVER(PARTITION BY customer_id, (CASE WHEN member = 'Y' THEN 1 END) ORDER BY order_date)
+			THEN RANK() OVER(PARTITION BY customer_id, (CASE WHEN member = 'Y' THEN 1 END)
+				ORDER BY order_date)
 		ELSE NULL
 	END AS [ranking]
 FROM cte
